244c1e584f2802cf79b541ccfa86a4ce1fbb481e
Hamilton Tavares <hamilton.hft@gmail.com>
2021-11-05 18:39:22 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
-  return (
-    <div> 
-      <h1>Olá Mundo!</h1>     
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <div> 
+        <h1 className = "text-primary" >Olá Mundo!!</h1>     
+      </div>
+    );
+  }
+  
+  export default App;
+  
